432241e094edbf9f0b6885f88e4d62f79cf538a2
Peter Nigh <peternigh@Peters-MacBook-Pro.local>
2020-09-28 16:42:48 -0500

    MVP

--- js/index.js
+++ js/index.js
@@ -69,13 +69,59 @@ newButton.textContent = 'Get Started';
 let ctaImage = document.querySelector('#cta-img')
 ctaImage.setAttribute('src', siteContent['cta']['img-src']);
 
-let mainContent = document.querySelector('.main-content');
+let mainContent = document.querySelector('.main-content').children;
 console.log(mainContent);
-let newTop = mainContent.querySelector('.top-content')
-console.log(newTop);
-let topText = newTop.querySelectorAll('.text-content')
-console.log(topText)
-let features = topText[0].children
 
-console.log(features)
-let about = topText[1].children
+let topContent = mainContent[0].children
+console.log(topContent)
+
+topContent[0].children[0].innerText = siteContent ['main-content'] ['features-h4'];
+
+topContent[0].children[1].innerText = siteContent ['main-content'] ['features-content']
+
+topContent[1].children[0].innerText = siteContent ['main-content'] ['about-h4']
+
+topContent[1].children[1].innerText = siteContent ['main-content'] ['about-content']
+
+let bottomContent = mainContent[2].children
+console.log(bottomContent)
+
+bottomContent[0].children[0].innerText = siteContent ['main-content'] ['services-h4']
+
+bottomContent[0].children[1].innerText = siteContent ['main-content'] ['services-content']
+
+bottomContent[1].children[0].innerText = siteContent ['main-content'] ['product-h4']
+
+bottomContent[1].children[1].innerText = siteContent ['main-content'] ['product-content']
+
+bottomContent[2].children[0].innerText = siteContent ['main-content'] ['vision-h4']
+
+bottomContent[2].children[1].innerText = siteContent ['main-content'] ['vision-content']
+
+
+let midImg = mainContent [1];
+
+midImg.src = siteContent ['main-content'] ['middle-img-src']
+
+let contact = document.querySelector('.contact').children;
+console.log(contact)
+
+contact[0].innerText = siteContent ['contact'] ['contact-h4']
+contact[1].innerText = siteContent ['contact'] ['address']
+contact[2].innerText = siteContent ['contact'] ['phone']
+contact[3].innerText = siteContent ['contact'] ['email']
+
+let footer = document.querySelector('footer')
+console.log(footer)
+
+footer.textContent = siteContent ['footer'] ['copyright']
+// let newTop = mainContent.querySelector('.top-content')
+// console.log(newTop);
+// let topText = newTop.querySelectorAll('.text-content')
+// console.log(topText)
+// let features = topText[0].children
+// // let featuresH4 = features.querySelector('h4');
+// console.log(features)
+// features[0].textContent = 'Features'
+// features[1].setAttribute('p', siteContent['main-content'[1]]);
+// let about = topText[1].children
